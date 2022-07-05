9e7d59c7f5ad838ac047756fdcf73300e3abd652
abkrew <silvernoobxd@gmail.com>
2022-07-05 17:39:58 +0300

    add display functionality

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+const buttons = document.querySelectorAll("button");
+const displayOperations =
+  document.getElementsByClassName("display-operation")[0];
+for (let i = 0; i < buttons.length; i++) {
+  if (buttons[i].textContent == "Clear") {
+    buttons[i].addEventListener("click", () => {
+      displayOperations.textContent = "";
+    });
+  } else if (buttons[i].textContent == "=") {
+    buttons[i].addEventListener("click", () => {
+      displayOperations.textContent = "";
+    });
+  } else {
+    buttons[i].addEventListener("click", () => {
+      if (displayOperations.textContent == "Enter a number") {
+        displayOperations.textContent = "";
+      }
+      displayOperations.textContent += buttons[i].textContent;
+    });
+  }
+}
+
+// function display(a) {
+//   const showText = a.textContent;
+//   console.log(a);
+//   displayOperations.textContent += showText;
+// }
+
 function add(a, b) {
   return +(a + b);
 }
