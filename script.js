70e95d91209fd7baf843877510921ee0b6465ff6
abkrew <silvernoobxd@gmail.com>
2022-07-06 13:08:47 +0300

    fix double operator and operator stops working bug

--- script.js
+++ script.js
@@ -16,6 +16,11 @@ numbers.forEach((number) => {
 });
 operators.forEach((operator) => {
   operator.addEventListener("click", () => {
+    if (value2 !== "") {
+      value1 = operate(value1, value2);
+      value2 = "";
+      currentOperator = operator.classList[1];
+    }
     value2 = value1;
     value1 = "";
     currentOperator = operator.classList[1];
@@ -26,26 +31,26 @@ equals.addEventListener("click", () => {
   operate(value1, value2);
 });
 
-function add(a, b) {
+function add(b, a) {
   return +(a + b);
 }
-function subtract(a, b) {
+function subtract(b, a) {
   return +(a - b);
 }
-function multiply(a, b) {
+function multiply(b, a) {
   return +(a * b);
 }
-function divide(a, b) {
+function divide(b, a) {
   return +(a / b);
 }
 function operate(num1, num2) {
   if (currentOperator == "add") {
-    displayOperations.textContent = add(+num1, +num2);
+    return (displayOperations.textContent = add(+num1, +num2));
   } else if (currentOperator == "subtract") {
-    displayOperations.textContent = subtract(+num1, +num2);
+    return (displayOperations.textContent = subtract(+num1, +num2));
   } else if (currentOperator == "multiply") {
-    displayOperations.textContent = multiply(+num1, +num2);
+    return (displayOperations.textContent = multiply(+num1, +num2));
   } else if (currentOperator == "divide") {
-    displayOperations.textContent = divide(+num1, +num);
+    return (displayOperations.textContent = divide(+num1, +num2));
   }
 }
