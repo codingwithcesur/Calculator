b537f9c40d25fc3e5cf17dcd20ffc0286ae98577
abkrew <silvernoobxd@gmail.com>
2022-07-06 12:27:08 +0300

    fix displaying functionality

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
-const buttons = document.querySelectorAll("button");
-const displayOperations =
-  document.getElementsByClassName("display-operation")[0];
-for (let i = 0; i < buttons.length; i++) {
-  if (buttons[i].textContent == "Clear") {
-    buttons[i].addEventListener("click", () => {
+const numbers = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".operator");
+const displayOperations = document.getElementById("display-operation");
+const equals = document.querySelector(".equal");
+let value1 = "";
+let value2 = "";
+let currentOperator = "";
+numbers.forEach((number) => {
+  number.addEventListener("click", () => {
+    if (displayOperations.textContent == "Enter a number") {
       displayOperations.textContent = "";
-    });
-  } else if (buttons[i].textContent == "=") {
-    buttons[i].addEventListener("click", () => {
-      displayOperations.textContent = "";
-    });
-  } else {
-    buttons[i].addEventListener("click", () => {
-      if (displayOperations.textContent == "Enter a number") {
-        displayOperations.textContent = "";
-      }
-      displayOperations.textContent += buttons[i].textContent;
-    });
-  }
-}
-
-// function display(a) {
-//   const showText = a.textContent;
-//   console.log(a);
-//   displayOperations.textContent += showText;
-// }
+    }
+    displayOperations.textContent += number.textContent;
+    value1 += number.textContent;
+  });
+});
+operators.forEach((operator) => {
+  operator.addEventListener("click", () => {
+    value2 = value1;
+    value1 = "";
+    currentOperator = operator.classList[1];
+    displayOperations.textContent += operator.textContent;
+  });
+});
+equals.addEventListener("click", () => {
+  operate(value1, value2);
+});
 
 function add(a, b) {
   return +(a + b);
@@ -38,6 +38,14 @@ function multiply(a, b) {
 function divide(a, b) {
   return +(a / b);
 }
-function operate(operator, num1, num2) {
-  return operator(+num1, +num2);
+function operate(num1, num2) {
+  if (currentOperator == "add") {
+    displayOperations.textContent = add(+num1, +num2);
+  } else if (currentOperator == "subtract") {
+    displayOperations.textContent = subtract(+num1, +num2);
+  } else if (currentOperator == "multiply") {
+    displayOperations.textContent = multiply(+num1, +num2);
+  } else if (currentOperator == "divide") {
+    displayOperations.textContent = divide(+num1, +num);
+  }
 }
